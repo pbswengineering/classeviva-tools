a2693796b223055369d4c1515bc445e97ecdd863
Paolo Bernardi <paolo@bernardi.cloud>
2022-11-05 23:52:03 +0100

    Grades comparison popup

--- grades.py
+++ grades.py
@@ -49,6 +49,7 @@ if __name__ == "__main__":
       <script src="//html5shiv.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
     <![endif]-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
     h1 {{
         margin-top: 1em;
@@ -68,14 +69,16 @@ if __name__ == "__main__":
   </div>
 </nav>
   <div class="container text-center">
-    <h1>Bad grades by subject</h1>
+    <h1>Insufficient grades by subject</h1>
     <div class="row row-cols-1 g-4">
         <canvas id="bad-grades-by-subject"></canvas>
     </div>
     <h1>Student performance</h1>
 """)
         COLS_PER_ROW = 3
+        students = []
         for i, sg in enumerate(student_grades):
+            students.append(f"{i+1}. {sg.student.name.upper()}")
             if (i % COLS_PER_ROW) == 0:
                 f.write(f'<div class="row row-cols-md-1 row-cols-lg-{COLS_PER_ROW} g-4">')
             css_border = ""
@@ -99,7 +102,10 @@ if __name__ == "__main__":
             f.write(f"""
 <div class="col">
     <div class="card{css_border}{css_text}">
-        <div class="card-header{css_border}{css_text}">{i+1}. {sg.student.name.upper()}</div>
+        <div class="card-header{css_border}{css_text}">
+            {i+1}. {sg.student.name.upper()}
+            <i class="bi bi-zoom-in" onclick="zoomGrades({i}, '{i+1}. {sg.student.name.upper()}')"></i>
+        </div>
         <div class="card-body{css_border}{css_text}"><canvas id="student_{i}"></canvas></div>
         <div class="card-footer{css_border}{css_text}">
         Average: {avg_str}{bad_str} 
@@ -112,11 +118,37 @@ if __name__ == "__main__":
         # Close the last row, if needed
         if ((len(student_grades) - 1) % COLS_PER_ROW) != (COLS_PER_ROW - 1):
             f.write("</div>")
+        students_js = "[" + ",".join(f'"{x}"' for x in students) + "]";
         f.write("""
 </div> <!-- .container -->
+<div class="modal fade" id="gradesModal" tabindex="-1" aria-labelledby="gradesModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="gradesModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Compare with...
+        </button>&nbsp;&nbsp;&nbsp;<span id="gradesModalCompareLabel"></span>
+        <ul class="dropdown-menu">""" +
+        "\n".join(f'''<li><a class="dropdown-item" href="#" onclick="compareWith({i}, '{x}')">{x}</a></li>''' for i, x in enumerate(students)) +
+        """</ul></div>
+        <canvas id="gradesModalCanvas"></canvas>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
+let students = """ + students_js + """
 function gradePointColor(ctx) {
     if (ctx.raw > 0 && ctx.raw < 5.5)
         return 'rgb(255, 99, 132)';
@@ -128,6 +160,56 @@ function gradePointRadius(ctx) {
         return 8;
     else
         return 4;
+}
+function studentChart(i, cssId, compare) {
+    currentStudent = i;
+    let d = JSON.parse(JSON.stringify(data[i])); // deep copy
+    d.datasets[0]["pointBackgroundColor"] = gradePointColor;
+    d.datasets[0]["pointRadius"] = gradePointRadius;
+    if (typeof compare !== "undefined") {
+        let newd = JSON.parse(JSON.stringify(data[compare].datasets[0])); // deep copy
+        newd["pointBackgroundColor"] = gradePointColor;
+        newd["pointRadius"] = gradePointRadius;
+        newd["backgroundColor"] = 'rgba(255, 99, 132, 0.2)';
+        newd["borderColor"] = 'rgb(255, 99, 132)';
+        newd["pointHoverBorderColor"] = 'rgb(255, 99, 132)';
+        d.datasets.push(newd);
+    }
+    let config = {
+        type: 'radar',
+        data: d,
+        options: {
+            elements: {line: {borderWidth: 3}},
+            plugins: {
+                legend: {display: false}
+            },
+            scales: {
+                r: {
+                    suggestedMin: 0,
+                    suggestedMax: 10
+                }
+            },
+        }
+    };
+    return new Chart(document.getElementById(cssId), config);
+}
+function destroyGradesModalChart() {
+    if (typeof gradesModalChart !== "undefined")
+        gradesModalChart.destroy();
+}
+function compareWith(i, name) {
+    destroyGradesModalChart();
+    gradesModalChart = studentChart(currentStudent, "gradesModalCanvas", i);
+    let gradesModalCompareLabel = document.getElementById("gradesModalCompareLabel");
+    gradesModalCompareLabel.innerHTML = name;
+}
+function zoomGrades(i, title) {
+    destroyGradesModalChart();
+    var gradesModal = new bootstrap.Modal(document.getElementById("gradesModal"), {});
+    gradesModal.show();
+    gradesModalChart = studentChart(i, 'gradesModalCanvas');
+    let gradesModalLabel = document.getElementById("gradesModalLabel");
+    gradesModalLabel.innerHTML = title;
 }
 let data = [];
 """)
@@ -152,23 +234,7 @@ data.push({
 """)
         f.write("""
 for (let i = 0; i < data.length; ++i) (function () {
-    let config = {
-        type: 'radar',
-        data: data[i],
-        options: {
-            elements: {line: {borderWidth: 3}},
-            plugins: {
-                legend: {display: false}
-            },
-            scales: {
-                r: {
-                    suggestedMin: 0,
-                    suggestedMax: 10
-                }
-            },
-        }
-    };
-    new Chart(document.getElementById('student_' + i), config);
+    studentChart(i, 'student_' + i);
 })();
 new Chart(document.getElementById("bad-grades-by-subject"), {
     type: 'bar',
